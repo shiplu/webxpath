4e68071cd27448f9487229ad37ca322e9ab9908f
Shiplu Mokaddim <shiplu.net@gmail.com>
2020-04-26 01:42:24 +0200

    create fields automatically from class properties

--- parser.py
+++ parser.py
@@ -1,8 +1,24 @@
 import webquery
 from lxml import etree
+import inspect
+from expression import Expression
+from collections import defaultdict
 
 
 class Parser(object):
+    registry = defaultdict(dict)
+
+    @classmethod
+    def __init_subclass__(cls):
+        for name, member in inspect.getmembers(cls):
+            if isinstance(member, Expression):
+                cls.registry[cls.__name__][name] = member
+
+    @property
+    def fields(self):
+        cls = self.__class__
+        return cls.registry[cls.__name__]
+
     def parse(self, url):
         content = webquery.urlcontent(url)
         root = etree.HTML(content, base_url=url)
