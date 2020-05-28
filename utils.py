82d6b50037a34c998d741689bbd76efae694c1aa
Shiplu Mokaddim <shiplu.net@gmail.com>
2020-05-28 17:05:53 +0200

    Add utils

---
+++ utils.py
@@ -0,0 +1,11 @@
+import requests
+
+
+class HttpSession(requests.Session):
+    def __init__(self, *args, **kwargs):
+        self.timeout = kwargs.pop('timeout', None)
+        super().__init__(*args, **kwargs)
+
+    def request(self, *args, **kwargs):
+        kwargs.setdefault('timeout', self.timeout)
+        return super().request(*args, **kwargs)
