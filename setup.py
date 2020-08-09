8977f320979998c9f18cfa7629c1811c7082dddf
Shiplu Mokaddim <shiplu.net@gmail.com>
2020-08-09 18:29:48 +0200

    Add setup.py (sigh)

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="webxpath",  # Replace with your own username
+    version="0.0.2",
+    author="Shiplu Mokaddim",
+    author_email="shiplu@mokadd.im",
+    description="Run XPath query and expressions against websites",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shiplu/webxpath",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
